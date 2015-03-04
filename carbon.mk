9e404a568faf5183e63086d00ec8f2e48d36cb02
STELIX <ssspinni@gmail.com>
2015-03-04 21:48:25 +0100

    M7 - Carbonizing AICP
    Change-Id: Iaf34aecc1f3a2149a6c1200147a82ab77a9a50ac
    Signed-off-by: STELIX <ssspinni@gmail.com>

---
+++ carbon.mk
@@ -0,0 +1,11 @@
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+$(call inherit-product, device/htc/m7/full_m7.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_ID=KTU84L.H4 \
+    BUILD_FINGERPRINT="htc/m7_google/m7:4.4.3/KTU84L.H4/347097:user/release-keys" \
+    PRIVATE_BUILD_DESC="4.07.1700.4 CL347097 release-keys"
+
+PRODUCT_NAME := carbon_m7
+PRODUCT_DEVICE := m7
